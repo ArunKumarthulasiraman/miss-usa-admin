e96fbf653de7232d47c68e849dfdb938c8f25015
yogaraja <yogaraja.subbiah@softsuave.com>
2021-06-30 16:42:08 +0530

    added useFetch and usePost custom hooks

---
+++ src/hooks/usePost/index.ts
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import axios, { AxiosResponse, AxiosError } from "axios";
+
+interface Props {}
+
+const usePost = (props: Props) => {
+  const token = "";
+  const [postData, setPostData] = useState<object>({
+    loading: false,
+    response: undefined,
+    error: "",
+  });
+
+  const executeApiCall = (url: string, data: any) => {
+    setPostData({ loading: true, response: undefined, error: "" });
+    axios
+      .post(
+        url,
+        {
+          Headers: { Authorization: token },
+        },
+        data
+      )
+      .then((response: AxiosResponse) => {
+        console.log(response.data);
+        setPostData({
+          loading: false,
+          response: response.data,
+          error: "",
+        });
+      })
+      .catch((error: AxiosError) => {
+        console.log(error);
+        setPostData({
+          loading: false,
+          response: undefined,
+          error: error.message,
+        });
+      });
+  };
+
+  return { ...postData, executeApiCall };
+};
+
+export default usePost;
